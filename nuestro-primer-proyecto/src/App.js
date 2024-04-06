6c0b0b2d21a34388a33c70c1b4b00ae6d6534eec
valentinaquispe1204 <vquispe@udesa.edu.ar>
2024-04-06 00:11:03 -0300

    cree los componenes de header y home

--- nuestro-primer-proyecto/src/App.js
+++ nuestro-primer-proyecto/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
-    <p> React </p>
+    
+    <Header/>
+    
+    
   );
+ 
 }
 
 export default App;
---
+++ nuestro-primer-proyecto/src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+    return(  
+        <header>
+    <div>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>Favoritos</a></li>
+            <li><a>Peliculas mas populares</a></li>
+        </ul>
+    </div>
+    </header>
+    );
+}
+
+export default Header
