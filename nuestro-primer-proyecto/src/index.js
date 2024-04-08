12cd691c5ecae40f5c9c493fadb310cc1586551a
valentinaquispe1204 <vquispe@udesa.edu.ar>
2024-04-08 01:46:32 -0300

    cree componente en home

--- nuestro-primer-proyecto/src/index.js
+++ nuestro-primer-proyecto/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
 );
 
 /* traemos el root de index.html y el root.render se encarga de renderizar la informacionn a index.html*/
--- nuestro-primer-proyecto/src/screens/Home/Home.js
+++ nuestro-primer-proyecto/src/screens/Home/Home.js
@@ -1,10 +1,35 @@
-import React from 'react'
-function Home() {
-  return (
-    <div>
-      <h1>bienvenidassssssss</h1>
-    </div>
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Search from "../../Components/Search/Search"
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrayDePelisPopulares: [],
+      arrayDePeliCartel : [],
+      loading: true,
+    };
+  }
+  componentDidMount() {
+  
+    console.log("Estamos en componentDidMount");
+    let urlPelisPopulares =
+    "https://api.themoviedb.org/3/movie/popular";
+  let urlPelisTop =
+  "https://api.themoviedb.org/3/movie/now_playing"
+  fetch(urlPelisPopulares)
+  .then((response) => response.json())
+  .then((data) =>
+    this.setState({
+      arrayDePelisPopulares: data.results,
+      loading: false,
+    })
   )
+  .catch((e) => console.log(e));
+
+  }
 }
+
 
 export default Home;
