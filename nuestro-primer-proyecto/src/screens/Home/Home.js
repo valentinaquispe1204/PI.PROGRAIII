48cc4cadf6aed3bd8bc2e96c0cb88bde963bb807
valentinaquispe1204 <vquispe@udesa.edu.ar>
2024-04-14 12:31:23 -0300

    home terminado

--- nuestro-primer-proyecto/src/screens/Home/Home.js
+++ nuestro-primer-proyecto/src/screens/Home/Home.js
@@ -16,28 +16,28 @@ class Home extends Component {
   }
   componentDidMount() {
     console.log("Estamos en componentDidMount");
-    // let urlPelisPopulares =
-    //   "https://api.themoviedb.org/3/movie/popular?api_key=04a9b8ef48334e7e5aecb64a2895739c";
-    // let urlPelisTop =
-    //   "https://api.themoviedb.org/3/movie/top_rated?api_key=04a9b8ef48334e7e5aecb64a2895739c";
-  //   fetch(urlPelisPopulares)
-  //     .then((response) => response.json())
-  //     .then((data) =>
-  //       this.setState({
-  //         arrayDePelisPopulares: data.results,
-  //         loading: false,
-  //       })
-  //     )
-  //     .catch((e) => console.log(e));
-  //   fetch(urlPelisTop)
-  //     .then((response) => response.json())
-  //     .then((data) =>
-  //       this.setState({
-  //         arrayDePelisTopRated: data.results,
-  //         loading: false,
-  //       })
-  //     )
-  //     .catch((e) => console.log(e));
+     let urlPelisPopulares =
+       "https://api.themoviedb.org/3/movie/popular?api_key=04a9b8ef48334e7e5aecb64a2895739c";
+    let urlPelisTop =
+       "https://api.themoviedb.org/3/movie/top_rated?api_key=04a9b8ef48334e7e5aecb64a2895739c";
+  fetch(urlPelisPopulares)
+     .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+         arrayDePelisPopulares: data.results,
+          loading: false,
+     })
+     )
+      .catch((e) => console.log(e));
+   fetch(urlPelisTop)
+      .then((response) => response.json())
+      .then((data) =>
+         this.setState({
+           arrayDePelisTopRated: data.results,
+         loading: false,
+      })
+       )
+     .catch((e) => console.log(e));
   }
   render() {
     return (
